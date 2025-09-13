bac96d48e671646597d98bf9dee803de626574ff
Damillora <developer@damillora.com>
2025-09-13 21:24:11 +0700

    fix: MOTD message on early boot

--- build_files/post-install.sh
+++ build_files/post-install.sh
@@ -5,3 +5,5 @@ systemctl enable podman.socket
 # Disable Zincati for now
 systemctl disable zincati.service
 
+# Disable OCI migration MOTD, we are already OCI
+systemctl disable coreos-oci-migration-motd.service
