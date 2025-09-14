be8ef544053315bf411dd9e80849bdfb8ce7392c
Damillora <developer@damillora.com>
2025-09-14 07:05:47 +0700

    feat: image signing

--- build_files/post-install.sh
+++ build_files/post-install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
 # Enable podman socket
 systemctl enable podman.socket
+
+# With that, disable docker socket to prevent accidental activation
+systemctl disable docker.socket
 
 # Disable Zincati for now
 systemctl disable zincati.service
 
 # Disable OCI migration MOTD, we are already OCI
 systemctl disable coreos-oci-migration-motd.service
+
+# Setup signing for Resna
+## Copy public key
+mkdir -p /etc/pki/containers
+cp /ctx/cosign.pub /etc/pki/containers/resna.pub
